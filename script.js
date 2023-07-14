c942a764668f06ec9c1da4538d2d04aaf417c4b3
CrabVS <139277442+CrabVS@users.noreply.github.com>
2023-07-14 23:02:02 +0200

    Add grid and cells

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('works');
+const createGrid = function createGrid(gridSize) {
+    const gridContainerEl = document.getElementById('grid-container');
+    gridContainerEl.innerHTML = '';
+
+    for (let index = 0; index < gridSize; index++) {
+        let newRow = document.createElement('div');
+        newRow.classList.add('grid-row');
+        for (let index = 0; index < gridSize; index++) {
+            let newCell = document.createElement('div');
+            newCell.classList.add('cell');
+            newRow.appendChild(newCell);
+        }
+
+        gridContainerEl.appendChild(newRow);
+    }
+}
+
+createGrid(16);
